bce8f22d9922c6dc80d0a67b08696ae64ceec3d7
Anthony <pyskell@users.noreply.github.com>
2020-05-17 12:48:06 -0400

    ADD: Mostly text improvements.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 import glob
 
-# glob.glob("/home/adam/*.txt") # file listing, unix-style
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Auto-grab templates
     tech_list = glob.glob('templates/tech/[!_]*.html')
     tech_list = list(map(lambda s : s.replace('templates/', '', 1), tech_list))
+
     return render_template('index.html', tech_list=tech_list)
