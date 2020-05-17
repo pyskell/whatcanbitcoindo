caad01e177258f45fe5d24639d7ecde9deebc46d
Anthony <pyskell@users.noreply.github.com>
2020-05-16 20:42:20 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import glob
+
+# glob.glob("/home/adam/*.txt") # file listing, unix-style
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
