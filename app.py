72f2e270a73de00e0462c1421292cc19de8d898b
Anthony <pyskell@users.noreply.github.com>
2020-05-18 13:24:58 -0400

    FIX: Sort files naturally.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from natsort import natsorted
 import glob
 
 app = Flask(__name__)
@@ -8,6 +9,6 @@ def home():
     # Auto-grab templates
     tech_list = glob.glob('templates/tech/[!_]*.html')
     tech_list = list(map(lambda s : s.replace('templates/', '', 1), tech_list))
-    tech_list.sort()
+    tech_list = natsorted(tech_list)
 
     return render_template('index.html', tech_list=tech_list)
