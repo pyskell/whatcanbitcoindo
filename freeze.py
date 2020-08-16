5a5a67e6d4605928c81652d915514d9ba123e8b6
Anthony <pyskell@users.noreply.github.com>
2020-08-16 11:01:23 -0400

    don't delete CNAME

--- freeze.py
+++ freeze.py
@@ -4,6 +4,7 @@ from flask_frozen import Freezer
 from app import app
 
 app.config['FREEZER_DESTINATION'] = 'docs'
+app.config['FREEZER_DESTINATION_IGNORE'] = ['CNAME']
 freezer = Freezer(app)
 
 if __name__ == '__main__':
